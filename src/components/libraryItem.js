99a64a2d7544dfc2c01ab9f20b485e32ce659014
muraguri64 <muraguri64@gmail.com>
2017-07-15 13:09:24 +0300

    Added libraryItem

---
+++ src/components/libraryItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { TouchableWithoutFeedback } from 'react-native';
+import { ListItem, Text, View } from 'native-base';
+import * as actions from '../actions';
+
+class Libraryitem extends Component {
+    renderDescription() {
+         if (this.props.item.id === this.props.selectLibraryId) {
+             return (
+                        <View padder style={{ flex: 1 }}>
+                            <Text>                           
+                                {this.props.item.description}
+                            </Text>
+                            <View 
+                                style={{ 
+                                        borderBottomColor: '#D3D3D3', 
+                                        borderBottomWidth: 0.7, 
+                                        marginTop: 10 
+                                      }} 
+                            />
+                        </View>
+                    );
+         }
+    }
+    render() {
+        return (
+            <View>                
+                <TouchableWithoutFeedback 
+                    onPress={() => this.props.selectLibrary(this.props.item.id)}
+                >
+                    <ListItem>
+                        <Text>{this.props.item.title}</Text>
+                    </ListItem>    
+                </TouchableWithoutFeedback>                
+                {this.renderDescription()}    
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = state => (
+        { selectLibraryId: state.selectLibraryId }
+    );
+
+export default connect(mapStateToProps, actions)(Libraryitem);
